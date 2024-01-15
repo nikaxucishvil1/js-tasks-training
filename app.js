6596200b3b0168e4ec9a40a3c9cf184a1121762c
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-01-15 20:54:47 +0400

    first2

---
+++ app.js
@@ -0,0 +1,37 @@
+//1 get third degree of triangle
+
+
+// function getThirdDeg(a,b) {
+//     let ThirdDeg = (180 - a - b)
+//     if(a >= 180 || b >= 180){
+//         console.log(`this type of triangle doesnt't exist`)
+//     }else if(a === `` || b === ``){
+//         console.log(`can't calculate`)
+//     }else if((parseInt(a) + parseInt(b)) >= 180){
+//         console.log(`this type of triangle doesnt't exist`)
+//     }
+//      else{
+//         console.log(ThirdDeg)
+//      }
+//      return getThirdDeg
+// }
+// console.log(getThirdDeg(prompt(`enter first deg`),prompt(`enter second deg`)))
+
+
+
+//2 find 3rd page of triangle with pithagoras theoreme
+
+// function getThirdPage(a,b) {
+//     let thirdPage = Math.sqrt((a*a) + (b*b))
+//     a !== 0
+//     b !== 0
+// if(a === `` || b === ``){
+//     console.log(`fill input properly`)
+// }else if(a <= 0 || b <= 0){
+//     console.log(`triangle doesn't exist`)
+// }else{
+//     console.log(thirdPage)
+// }
+
+// }
+// console.log(getThirdPage(prompt(`enter page length`),prompt`enter page length`))
